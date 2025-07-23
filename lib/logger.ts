08549169d3cfe8ecdeba4c97ea04f37bde85ce4a
asaber3030 <abdulrahmansaber70@gmail.com>
2025-07-23 12:38:10 +0300

    Refactor logger configuration to handle production and exception logging; remove unused test route files.

--- lib/logger.ts
+++ lib/logger.ts
@@ -6,11 +6,27 @@ const logFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`
 })
 
+// Check if the application is running in production
+const isProduction = process.env.NODE_ENV === "production"
+
+const loggerTransports = [
+  new transports.Console() // Console transport
+]
+
+// Configure exception handlers
+const exceptionHandlers = !isProduction
+  ? [
+      new transports.File({ filename: "logs/exceptions.log" }) // File transport for exceptions
+    ]
+  : [
+      new transports.Console() // Console transport for exceptions
+    ]
+
 const logger = createLogger({
   level: "info",
   format: combine(colorize(), timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), logFormat),
-  transports: [new transports.Console(), new transports.File({ filename: "logs/error.log", level: "error" }), new transports.File({ filename: "logs/combined.log" })],
-  exceptionHandlers: [new transports.File({ filename: "logs/exceptions.log" })]
+  transports: loggerTransports,
+  exceptionHandlers: exceptionHandlers
 })
 
 export default logger
