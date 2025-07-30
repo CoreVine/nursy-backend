6ab8755fe61c1032bd130a4c0161edd5d878a755
asaber3030 <abdulrahmansaber70@gmail.com>
2025-07-30 11:10:23 +0300

    Remove rate limiter service from the services array in register.ts

--- src/services/register.ts
+++ src/services/register.ts
@@ -2,6 +2,6 @@ import rateLimiterService from "./rate-limiter.service"
 import corsService from "./cors.service"
 import helmetService from "./helmet.service"
 
-const services = [rateLimiterService, corsService, helmetService]
+const services = [corsService]
 
 export default services
